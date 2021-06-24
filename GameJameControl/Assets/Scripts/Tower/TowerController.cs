0268097cd4a59bdaf9c15602ebc3238948200bb2
ben <bennybill16@gail.com>
2021-06-24 13:28:00 +1000

    Doing a loop timer for the time between hits on tower.

--- GameJameControl/Assets/Scripts/Tower/TowerController.cs
+++ GameJameControl/Assets/Scripts/Tower/TowerController.cs
@@ -6,14 +6,22 @@ using UnityEngine;
 /// </summary>
 public class TowerController : Singleton<TowerController>
 {
+    [SerializeField] private float timeBetweenHits = 2.0f;
+    private float dt = 0.0f;
     public int health = 3;
     private void Update()
     {
+        dt += Time.deltaTime;
         if (health <= 0)
         {
             //Do Something
         }
-        else if (EnemyController.Instance.isHitTower)
+        else if (EnemyController.Instance.isHitTower && dt >= timeBetweenHits)
+        {
+            dt = 0;
             health--;
+        }
+        else if (dt > timeBetweenHits)
+            dt = 0;
     }
 }
