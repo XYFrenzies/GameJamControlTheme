5a8d4a91a39ec3eeee5f163d27fdaef1964b1680
Igor Doslov <s191719@students.aie.edu.au>
2021-06-24 12:37:07 +1000

    Player movement. Doesnt rotate yet

--- GameJameControl/Assets/Scripts/PlayerController/PlayerController.cs
+++ GameJameControl/Assets/Scripts/PlayerController/PlayerController.cs
@@ -1,17 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+    private Vector2 m_Move;
+
+
+
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        m_Move = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
+    {
+
+        Move(m_Move);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01)
+            return;
+        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+        // For simplicity's sake, we just keep movement in a single plane here. Rotate
+        // direction according to world Y rotation of player.
+        var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
+        transform.position += move * scaledMoveSpeed;
+    }
+
+    private void DodgeRoll()
     {
         
     }
