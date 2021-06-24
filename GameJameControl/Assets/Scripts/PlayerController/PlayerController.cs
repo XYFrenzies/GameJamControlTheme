ee27d3dbafb421eadee1211b7e25198f2715f45b
Igor Doslov <s191719@students.aie.edu.au>
2021-06-24 16:47:03 +1000

    Dodge WIP

--- GameJameControl/Assets/Scripts/PlayerController/PlayerController.cs
+++ GameJameControl/Assets/Scripts/PlayerController/PlayerController.cs
@@ -7,11 +7,16 @@ using System;
 public class PlayerController : Singleton<PlayerController>
 {
     public float moveSpeed;
+    private float initialMoveSpeed;
+    public float maxSpeed;
     public float dodgeSpeed;
     private Vector2 m_Move;
     private Rigidbody rb;
+    public bool canDodge;
+    public float dodgeTimer = 0f;
+    public float dodgeTime = 3f;
 
-   // private Quaternion initialRot;
+    // private Quaternion initialRot;
 
 
     public int health = 3;
@@ -23,33 +28,51 @@ public class PlayerController : Singleton<PlayerController>
 
     public void OnDodgeRoll(InputAction.CallbackContext context)
     {
+
+        if (context.phase != InputActionPhase.Performed)
+        {
+            return;
+        }
         DodgeRoll();
 
     }
 
     private void Start()
     {
-        //initialRot = transform.rotation;
+        initialMoveSpeed = moveSpeed;
         rb = GetComponent<Rigidbody>();
     }
 
     public void Update()
     {
         Move(m_Move);
-        //DodgeRoll();
+        dodgeTimer += Time.deltaTime;
+        if (dodgeTimer >= dodgeTime)
+        {
+            canDodge = true;
+
+        }
+        else
+        {
+            moveSpeed = initialMoveSpeed;
+            canDodge = false;
+        }
     }
 
     private void Move(Vector2 direction)
     {
         if (direction.sqrMagnitude < 0.01)
             return;
-        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+        var scaledMoveSpeed = initialMoveSpeed * Time.deltaTime;
         // For simplicity's sake, we just keep movement in a single plane here. Rotate
         // direction according to world Y rotation of player.
         //var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
         //transform.position += new Vector3(direction.x, 0, direction.y) * scaledMoveSpeed;
         //float angle = Vector3.SignedAngle(new Vector3(direction.x, 0, direction.y), transform.forward, Vector3.up);
-
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
         Vector3 force = new Vector3(direction.x, 0, direction.y) * scaledMoveSpeed;
         rb.AddForce(force);
         //transform.right = direction;
@@ -63,7 +86,11 @@ public class PlayerController : Singleton<PlayerController>
 
     private void DodgeRoll()
     {
-        //moveSpeed *= dodgeSpeed;
-        Debug.Log("dodge");
+        if (canDodge)
+        {
+            moveSpeed = dodgeSpeed;
+            Debug.Log(moveSpeed);
+            dodgeTimer = 0f;
+        }
     }
 }
