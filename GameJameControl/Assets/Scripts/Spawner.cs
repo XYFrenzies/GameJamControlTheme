fd5c5636de1949c54e9e7b36ffa964cb8841ecf2
Igor Doslov <s191719@students.aie.edu.au>
2021-06-24 15:38:49 +1000

    Spawner script

---
+++ GameJameControl/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject objToSpawn;
+    public float repeatRateMin;
+    public float repeatRateMax;
+    public float startTime;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObj", startTime, Random.Range(repeatRateMin, repeatRateMax));
+    }
+
+    void SpawnObj()
+    {
+        Instantiate(objToSpawn, gameObject.transform.position, gameObject.transform.rotation);
+    }
+}
