6e46227d5a3b80ef631dde148824c5d2f7ed49c4
Ben <bennybill16@gmail.com>
2021-06-25 11:34:29 +1000

    I have made a main menu scene and have changed the name of the mainscene
    I have put in the different assets into the game.

--- GameJameControl/Assets/Scripts/UI/GameState.cs
+++ GameJameControl/Assets/Scripts/UI/GameState.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameState : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame() 
     {
-        
+        SceneManager.LoadScene("MainGame");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void QuitGame() 
     {
-        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
