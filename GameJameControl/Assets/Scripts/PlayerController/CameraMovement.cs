d0a277e6063ff64efd4713f0750fec56a4246f39
Igor Doslov <s191719@students.aie.edu.au>
2021-06-25 15:40:22 +1000

    stuff

--- GameJameControl/Assets/Scripts/PlayerController/CameraMovement.cs
+++ GameJameControl/Assets/Scripts/PlayerController/CameraMovement.cs
@@ -1,23 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[ExecuteAlways]
+
+[ExecuteInEditMode]
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private Transform m_player = null;
-    [SerializeField] private Vector3 offset = Vector3.zero;
-    [SerializeField] private Vector3 rotation = Vector3.zero;
-    private void Start()
+    public GameObject target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
     {
-        offset = gameObject.transform.position;
 
     }
+
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        transform.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
-        Vector3 cameraPos = m_player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, cameraPos, 1);
-        transform.position = smoothedPosition;
+        transform.position = target.transform.position + offset;
     }
 }
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//[ExecuteAlways]
+//public class CameraMovement : MonoBehaviour
+//{
+//    [SerializeField] private Transform m_player = null;
+//    [SerializeField] private Vector3 offset = Vector3.zero;
+//    [SerializeField] private Vector3 rotation = Vector3.zero;
+//    private void Start()
+//    {
+//        offset = gameObject.transform.position;
+
+//    }
+//    // Update is called once per frame
+//    void LateUpdate()
+//    {
+//        transform.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
+//        Vector3 cameraPos = m_player.position + offset;
+//        Vector3 smoothedPosition = Vector3.Lerp(transform.position, cameraPos, 1);
+//        transform.position = smoothedPosition;
+//    }
+//}
